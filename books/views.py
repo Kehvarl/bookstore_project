dbb57a97d0d187d9e2380e133cce6673d2a88139
Kehvarl <kehvarl@kehvarl.com>
2021-03-23 17:46:27 -0700

    pep8

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.views.generic import ListView, DetailView
 
 from .models import Book
