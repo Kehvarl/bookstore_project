9986baa9feb126d52975dceb4ad7f1c67601a32c
Kehvarl <kehvarl@kehvarl.com>
2021-03-29 18:46:29 -0700

    xss remediation strategies

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -14,6 +14,9 @@ import os
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
